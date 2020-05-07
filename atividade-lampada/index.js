622dc0401ec7cb85ac48cee77677f165e324f6f5
Jefferson Martins de Andrade <jeffersonmandrade@gmail.com>
2020-05-07 01:37:12 -0300

    implementado função de desligar

--- atividade-lampada/index.js
+++ atividade-lampada/index.js
@@ -1,22 +1,37 @@
 function ligar() {
-    //pegar elemento de imagem
+    // pegar elemento de imagem
     const lampada = document.querySelector("#lampada")
     //trocar src para ligada
     lampada.src = "img/lampada-ligada.jpeg"
     //trocar alt para ligada
     lampada.alt = "Lâmpada ligada"
 
-    //pegar elemento button
+    //pegar elemento button de ligar
     const botaoLigar = document.querySelector("#botao-ligar")
     //adicionar atributo disabled
     botaoLigar.disabled = true
 
-    //pegar elemento button
+    //pegar elemento button de desligar
     const botaoDesligar = document.querySelector("#botao-desligar")
     //remover atributo disabled
     botaoDesligar.disabled = false
 }
 
 function desligar() {
+    // pegar elemento de imagem
+    const lampada = document.querySelector("#lampada")
+    //trocar src para desligada
+    lampada.src = "img/lampada-desligada.jpeg"
+    //trocar alt para desligada
+    lampada.alt = "Lâmpada desligada"
 
+    //pegar elemento button de ligar
+    const botaoLigar = document.querySelector("#botao-ligar")
+    //remover atributo disabled
+    botaoLigar.disabled = false
+
+    //pegar elemento button de desligar
+    const botaoDesligar = document.querySelector("#botao-desligar")
+    //adicionar atributo disabled
+    botaoDesligar.disabled = true
 }
