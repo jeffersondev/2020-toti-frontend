4fde32c8c319bbb6345ff3e38c6a8682ceb7511f
Jefferson Martins de Andrade <jeffersonmandrade@gmail.com>
2020-05-11 23:58:25 -0300

    incrementando atividade

--- atividade-lista-tarefas/index.js
+++ atividade-lista-tarefas/index.js
@@ -3,10 +3,13 @@ const botaoAdicionar = document.querySelector("#add-task-button")
 const listaDeTarefas = document.querySelector(".task-list")
 
 function adicionarTarefa(event) {
+    event.preventDefault()
+    if (novaTarefaInput.checkValidity() === false) {
+        return;
+    }
     const novoItem = document.createElement("li")
     novoItem.textContent = novaTarefaInput.value
     novoItem.classList.add("task-list__item")
-    novoItem.appendChild(botao)
     listaDeTarefas.appendChild(novoItem)
     novaTarefaInput.value = ""
 }
