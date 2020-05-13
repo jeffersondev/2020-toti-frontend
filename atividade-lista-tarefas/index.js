fcbd078921048de2acc94b84d2cf46655c1f341d
Jefferson Martins de Andrade <jeffersonmandrade@gmail.com>
2020-05-13 19:06:28 -0300

    incremento da atividade de lista de tarefas

--- atividade-lista-tarefas/index.js
+++ atividade-lista-tarefas/index.js
@@ -6,12 +6,59 @@ function adicionarTarefa(event) {
     if (novaTarefaInput.value === "") {
         return;
     }
-    const novoItem = document.createElement("li")
-    novoItem.textContent = novaTarefaInput.value
-    novoItem.classList.add("task-list__item")
+    const novoItem = criarNovoItem(novaTarefaInput.value)
     listaDeTarefas.appendChild(novoItem)
     novaTarefaInput.value = ""
     novaTarefaInput.focus()
+}
+
+function criarNovoItem(textoTarefa) {
+    const novoItem = document.createElement("li")
+    novoItem.innerText = textoTarefa
+    novoItem.classList.add("task-list__item")
+    novoItem.appendChild(criarBotaoCompletarTarefa())
+    return novoItem
+}
+
+function criarBotaoCompletarTarefa() {
+    const botao = document.createElement("button")
+    botao.type = "button"
+    botao.classList.add("task-list__action")
+    botao.innerHTML = "<i class=\"fas fa-check-square\"></i>"
+    botao.addEventListener("click", completarTarefa)
+    return botao
+}
+
+function completarTarefa(event) {
+    const botao = event.currentTarget;
+    const item = botao.parentNode
+
+    //riscar item
+    item.classList.remove("task-list__item")
+    item.classList.add("task-list__item--done")
+
+    //mudar ícone
+    botao.innerHTML = "<i class=\"fas fa-undo-alt\"></i>"
+
+    //atualiza funções chamadas
+    botao.removeEventListener("click", completarTarefa)
+    botao.addEventListener("click", desfazerTarefa)
+}
+
+function desfazerTarefa(event) {
+    const botao = event.currentTarget;
+    const item = botao.parentNode
+
+    //riscar item
+    item.classList.remove("task-list__item--done")
+    item.classList.add("task-list__item")
+
+    //mudar ícone
+    botao.innerHTML = "<i class=\"fas fa-check-square\"></i>"
+
+    //atualiza funções chamadas
+    botao.removeEventListener("click", desfazerTarefa)
+    botao.addEventListener("click", completarTarefa)
 }
 
 botaoAdicionar.addEventListener("click", adicionarTarefa)
